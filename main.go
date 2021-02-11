b1496c81e2f5b98d1ad7a4133ba94ea7d7de4143
Tom Arrell <tom.arrell@gmail.com>
2021-02-11 21:50:48 +0100

    Add log level flag

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ const (
 	defaultCertPath      = "./testdata/localhost.pem"
 	defaultKeyPath       = "./testdata/localhost-key.pem"
 	defaultDBPath        = "./miniqueue"
+	defaultLogLevel      = "debug"
 )
 
 func main() {
@@ -26,6 +27,7 @@ func main() {
 		tlsCertPath   = flag.String("cert", defaultCertPath, "path to TLS certificate")
 		tlsKeyPath    = flag.String("key", defaultKeyPath, "path to TLS key")
 		dbPath        = flag.String("db", defaultDBPath, "path to the db file")
+		logLevel      = flag.String("level", defaultLogLevel, "(disabled|debug|info)")
 	)
 
 	flag.Parse()
@@ -34,16 +36,30 @@ func main() {
 		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	}
 
+	switch *logLevel {
+	case "debug":
+		log.Logger = log.Level(zerolog.DebugLevel)
+	case "info":
+		log.Logger = log.Level(zerolog.InfoLevel)
+	case "disabled":
+		log.Logger = log.Level(zerolog.Disabled)
+	default:
+		log.Fatal().Msg("invalid log level, see -h")
+	}
+
 	if *dbPath == defaultDBPath {
-		log.Warn().Msgf("no DB path specified, using default %s", defaultDBPath)
+		log.Warn().
+			Msgf("no DB path specified, using default %s", defaultDBPath)
 	}
 
 	if *tlsCertPath == defaultCertPath {
-		log.Warn().Msgf("no TLS certificate path specified, using default %s", defaultCertPath)
+		log.Warn().
+			Msgf("no TLS certificate path specified, using default %s", defaultCertPath)
 	}
 
 	if *tlsKeyPath == defaultKeyPath {
-		log.Warn().Msgf("no TLS key path specified, using default %s", defaultKeyPath)
+		log.Warn().
+			Msgf("no TLS key path specified, using default %s", defaultKeyPath)
 	}
 
 	srv := newServer(newBroker(newStore(*dbPath)))
@@ -56,6 +72,8 @@ func main() {
 		Msg("starting miniqueue")
 
 	if err := http.ListenAndServeTLS(p, *tlsCertPath, *tlsKeyPath, srv); !errors.Is(err, http.ErrServerClosed) {
-		log.Fatal().Err(err).Msg("server closed")
+		log.Fatal().
+			Err(err).
+			Msg("server closed")
 	}
 }
